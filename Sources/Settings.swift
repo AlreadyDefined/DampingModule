5dc00705316b56a2ddd55d65eb6d50cef90d9348
Ирина Филиппова <4956083+AlreadyDefined@users.noreply.github.com>
2018-05-01 00:00:08 +0300

    Update Settings.swift

--- Sources/Settings.swift
+++ Sources/Settings.swift
@@ -4,7 +4,7 @@ public class Settings {
     public static var T = 3.0
     public static var K = 10
     public static var M = 5
-    public static var N = 800
+    public static var N = 600
     
     public static var R = 1.0
     
