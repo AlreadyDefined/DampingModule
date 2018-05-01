bd5b9c515fb5950f0b5cbfef0db9aa02d70e50c0
Ирина Филиппова <4956083+AlreadyDefined@users.noreply.github.com>
2018-05-01 21:15:48 +0300

    Update Settings.swift

--- Sources/Settings.swift
+++ Sources/Settings.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public class Settings {
-    public static var T = 4.0
+    public static var T = 3.0
     public static var K = 10
     public static var M = 5
     public static var N = 250
