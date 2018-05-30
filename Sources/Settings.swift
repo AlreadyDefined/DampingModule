948426519fbb01d093bdfdcafbba6a5f9a47c744
Ирина Филиппова <4956083+AlreadyDefined@users.noreply.github.com>
2018-05-30 12:58:14 +0300

    Update Settings.swift

--- Sources/Settings.swift
+++ Sources/Settings.swift
@@ -12,7 +12,7 @@ public class Settings {
     public static var T = 3.0
     public static var K = 5
     public static var M = 5
-    public static var N = 900
+    public static var N = 700
     
     public static var R = 1.0
     
@@ -24,7 +24,7 @@ public class Settings {
     
     public static var Accuracy = 0.01
     
-    public static var ActuatorType = ActType.point
+    public static var ActuatorType = ActType.circular
     
     public static func h_r() -> Double {
         return Double(R) / (Double(M) - 0.5)
